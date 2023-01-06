ddf5c9934ce96acd8247de9dd9890725466b3152
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-01-06 21:07:13 +0900

    generic

---
+++ code/chapter02/part1/src/08-泛型.ts
@@ -0,0 +1,41 @@
+/*
+function fn(a:number):number {
+    return a
+}*/
+
+/*
+ * 在定义函数或是类时，如果遇到类型不明确的，就可以使用泛型
+ *  泛型的作用就是，当类型不明确时，整一个变量，用变量表示泛型
+ */
+function fn<T>(a:T):T{
+    return a
+}
+
+//可以直接调用具有泛型的函数
+let result=fn(10)  //不指定泛型，TS可以自动对类型进行推断
+let result2=fn<string>('hello')  //指定泛型
+
+//泛型可以同时指定多个
+function fn2<T,K>(a:T,b:K):T {
+    console.log(b);
+    return a
+}
+fn2<number,string>(23,'bye~')
+
+interface Inter{
+    length:number
+}
+
+//T extends Inter  表示泛型T必须是Inter实现类(子类)
+function fn3<T extends Inter>(a:T):number {
+    return a.length
+}
+
+class MyClass<T> {
+    name:T
+    constructor(name:T) {
+        this.name=name
+    }
+}
+const mc=new MyClass<string>('Vilhelm')
+
