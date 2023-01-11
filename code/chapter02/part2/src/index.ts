22d2258e52f494fcd9274504b29e062824d9afe0
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-01-11 16:38:02 +0900

    gamecontrol

--- code/chapter02/part2/src/index.ts
+++ code/chapter02/part2/src/index.ts
@@ -1,10 +1,14 @@
 //引入样式
 import './style/index.less'
-import Food from './modules/Food';
+// import Food from './modules/Food';
+// const food=new Food()
+// console.log(food.X,food.Y)
+// food.change()
+// console.log(food.X,food.Y)
 
-const food=new Food()
-console.log(food.X,food.Y)
-food.change()
-console.log(food.X,food.Y)
+import GameControl from "./modules/GameControl";
 
-
+const gameControl=new GameControl()
+// setInterval(()=>{
+//     console.log(gameControl.direction)
+// },1000)
---
+++ code/chapter02/part2/src/modules/GameControl.ts
@@ -0,0 +1,87 @@
+//引入其他的类
+import Snake from "./Snake"
+import Food from "./Food"
+import ScorePanel from "./ScorePanel";
+
+//游戏的控制器，控制其他的所有类
+class GameControl{
+    //定义三个属性
+    //蛇
+    snake:Snake;
+    food:Food;
+    scorePanel:ScorePanel;
+
+    //创建一个属性来存储红色的移动方向(也就是按键的方向)
+    direction:string='';
+
+    constructor() {
+        this.snake=new Snake()
+        this.food=new Food()
+        this.scorePanel=new ScorePanel()
+
+        this.init()
+    }
+
+    //游戏的初始化方法，调用后游戏即开始
+    init(){
+        //绑定键盘按键按下的事件
+        document.addEventListener('keydown',this.keydownHandler.bind(this));
+    }
+
+    /*
+     * ArrowUp  Up
+     * ArrowDown  Down
+     * ArrowLeft  Left
+     * ArrowRight  Right
+     */
+
+    //创建一个键盘按下的响应函数
+    keydownHandler(event:KeyboardEvent){
+        //需要检查event.key的值是否合法(用户是否按了正确的按键)
+        //修改diretion属性
+        this.direction=event.key;
+    }
+
+    //创建一个控制蛇移动的方法
+    run(){
+        /*
+         * 根据方向(this.diretion)来使蛇的为之改变
+         * 向上 top 减少
+         * 向下 top 增加
+         * 向左 left 减少
+         * 向右 left 增加
+         */
+        //获取蛇现在的坐标
+        let X=this.snake.X;
+        let Y=this.snake.Y;
+
+        //根据按键的方向来修改X值和Y值
+        switch(this.direction){
+            case "ArrowUp":
+            case "Up":
+                //向上移动top 减少
+                Y-=10;
+                break;
+            case "ArrowDown":
+            case "Down":
+                //向下移动top 增加
+                Y+=10;
+                break;
+            case "ArrowLeft":
+            case "Left":
+                //向左移动left 减少
+                X-=10;
+                break;
+            case "ArrowRight":
+            case "Right":
+                //向右移动left 增加
+                X+=10;
+                break;
+        }
+        //修改蛇的X值和Y值
+        this.snake.X=X;
+        this.snake.Y=Y;
+    }
+}
+
+export default GameControl;
